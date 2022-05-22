67e6eed888f75a2548065cc0d97eb7d321d22421
poblemasso <96197746+poblemasso@users.noreply.github.com>
2022-05-22 17:57:27 +0200

    Arreglar errors i afegir min max

--- reducer.py
+++ reducer.py
@@ -3,6 +3,8 @@ import sys
 
 current_word = None
 current_count = 0
+max_len= 0
+min_len= 1000
 word = None
 
 # input comes from STDIN
@@ -12,9 +14,11 @@ for line in sys.stdin:
     line=line.lower()
 
     # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
+    word, count, max_word_len, min_word_len = line.split('\t')
     try:
       count = int(count)
+      max_word_len= int(max_word_len)
+      min_word_len= int(min_word_len)  
     except ValueError:
       #count was not a number, so silently
       #ignore/discard this line
@@ -23,14 +27,27 @@ for line in sys.stdin:
     # this IF-switch only works because Hadoop sorts map output
     # by key (here: word) before it is passed to the reducer
     if current_word == word:
+        if max_word_len>max_len:
+                max_len=max_word_len
+        if min_word_len<min_len:
+                min_len=min_word_len
         current_count += count
     else:
         if current_word:
+            if max_word_len>max_len:
+                max_len=max_word_len
+            if min_word_len<min_len:
+                min_len=min_word_len
             # write result to STDOUT
-            print ('%s\t%s' % (current_word, current_count))
+            print ('%s\t%s\t%s\t%s' % (current_word, current_count, max_len, min_len))
         current_count = count
         current_word = word
-
+        max_len=max_word_len
+        min_len=min_word_len
 # do not forget to output the last word if needed!
 if current_word == word:
-    print( '%s\t%s' % (current_word, current_count))
+    if max_word_len>max_len:
+        max_len=max_word_len
+    if min_word_len<min_len:
+        min_len=min_word_len
+    print ('%s\t%s\t%s\t%s' % (current_word, current_count, max_len, min_len))
